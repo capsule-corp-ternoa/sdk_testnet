f698de8bc3c9190edfeef3e931b8bab2084a04c7
Mohsin Riaz <bahrianboy@gmail.com>
2022-02-10 05:14:39 +0500

    invalid seed validation for mp related queries

--- app/routes/marketPlaceRoutes.ts
+++ app/routes/marketPlaceRoutes.ts
@@ -31,12 +31,12 @@ import { checkMpOwnershipMiddleware } from '../middleware/marketplace';
 const marketplaceRouter = Router();
 
 marketplaceRouter.post("/api/createMarketPlace",validationMiddleware(createMarketPlaceSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.create), createMarketPlace);
-marketplaceRouter.post("/api/setCommissionFee",  validationMiddleware(setCommissionFeeSchema),checkMpOwnershipMiddleware,balanceCheckMiddleware(txPallets.marketplace, txActions.setCommissionFee), setCommissionFee);
-marketplaceRouter.post("/api/setOwnerForMarketPlace",  validationMiddleware(setOwnerFeeSchema),checkMpOwnershipMiddleware,balanceCheckMiddleware(txPallets.marketplace, txActions.setOwner), setOwnerForMarketPlace);
-marketplaceRouter.post("/api/setKindForMarketPlace",  validationMiddleware(setKindSchema),checkMpOwnershipMiddleware,balanceCheckMiddleware(txPallets.marketplace, txActions.setKind), setKindForMarketPlace);
-marketplaceRouter.post("/api/setNameForMarketPlace",  validationMiddleware(setNameSchema),checkMpOwnershipMiddleware,balanceCheckMiddleware(txPallets.marketplace, txActions.setName), setNameForMarketPlace);
-marketplaceRouter.post("/api/setUriForMarketPlace",  validationMiddleware(setUriSchema),checkMpOwnershipMiddleware,balanceCheckMiddleware(txPallets.marketplace, txActions.setUri), setUriForMarketPlace);
-marketplaceRouter.post("/api/setlogoUriForMarketPlace",  validationMiddleware(setLogoUriSchema),checkMpOwnershipMiddleware,balanceCheckMiddleware(txPallets.marketplace, txActions.setLogoUri), setlogoUriForMarketPlace);
+marketplaceRouter.post("/api/setCommissionFee",  validationMiddleware(setCommissionFeeSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.setCommissionFee),checkMpOwnershipMiddleware, setCommissionFee);
+marketplaceRouter.post("/api/setOwnerForMarketPlace",  validationMiddleware(setOwnerFeeSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.setOwner),checkMpOwnershipMiddleware, setOwnerForMarketPlace);
+marketplaceRouter.post("/api/setKindForMarketPlace",  validationMiddleware(setKindSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.setKind),checkMpOwnershipMiddleware, setKindForMarketPlace);
+marketplaceRouter.post("/api/setNameForMarketPlace",  validationMiddleware(setNameSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.setName),checkMpOwnershipMiddleware, setNameForMarketPlace);
+marketplaceRouter.post("/api/setUriForMarketPlace",  validationMiddleware(setUriSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.setUri),checkMpOwnershipMiddleware, setUriForMarketPlace);
+marketplaceRouter.post("/api/setlogoUriForMarketPlace",  validationMiddleware(setLogoUriSchema),balanceCheckMiddleware(txPallets.marketplace, txActions.setLogoUri),checkMpOwnershipMiddleware, setlogoUriForMarketPlace);
 marketplaceRouter.get("/api/getMarketplacesForOwner/:ownerAddress", validationMiddleware(MarketPlaceByOwnerSchema), getMarketplaceDataByOwner);
 marketplaceRouter.get("/api/getMarketplaceById/:id",  validationMiddleware(MarketPlaceByIdSchema), getMarketplaceById);
 marketplaceRouter.get("/api/getMarketplaceByIdfromChain/:id",  validationMiddleware(MarketPlaceByIdSchema), getMarketplaceByIdFromChain);
