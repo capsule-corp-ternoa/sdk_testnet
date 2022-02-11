73a24e56fe7add4e77f44c4fbc7ffc9883d8b96e
Mohsin Riaz <bahrianboy@gmail.com>
2022-02-11 22:31:53 +0500

    marketplace ownership error improve

--- app/middleware/marketplace.ts
+++ app/middleware/marketplace.ts
@@ -5,24 +5,33 @@ import { getUserFromSeed } from "../service/blockchain.service";
 import { getMarketplaceDataByIdFromBlockChain } from "../service/marketPlaceService";
 
 export const checkMpOwnershipMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const {mpId} = req.body;
-    const seed = getSeedFromRequest(req);
-    const user = await getUserFromSeed(seed);
-    const marketPlaceData=await getMarketplaceDataByIdFromBlockChain(mpId);
-    if(user && marketPlaceData)
-    {
-        if(marketPlaceData.owner===user.address)
+    try{
+        const {mpId} = req.body;
+        const seed = getSeedFromRequest(req);
+        const user = await getUserFromSeed(seed);
+        const marketPlaceData=await getMarketplaceDataByIdFromBlockChain(mpId);
+        if(user && marketPlaceData)
         {
-            next();
+            if(marketPlaceData.owner===user.address)
+            {
+                next();
+            }
+            else
+            {
+                res.status(403).send('Forbidden! Not a Marketplace Owner!')
+            }
         }
         else
         {
-            res.status(403).send('Forbidden! Not a Marketplace Owner!')
+            res.status(500).send("Data Fetch Error!")
         }
+    }catch(err){
+        console.log('error', err)
+        res.status(500).json({
+            message:'operation failed',
+            details:err && (err as any).message?(err as any).message:err
+        })
     }
-    else
-    {
-        res.status(500).send("Data Fetch Error!")
-    }
+   
     
 }
